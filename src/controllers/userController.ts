9b3dbc4aac1a0890498f0dafba7d2a49e04eeca1
elias <perexlito@gmail.com>
2022-10-31 12:48:34 -0300

    add getUsers controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -43,7 +43,20 @@ const login = async (req: Request, res: Response) => {
   }
 };
 
+const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await db.User.findAll();
+    if (!users) {
+      return res.status(404).json({ message: 'Not found users', status: 404 });
+    }
+    return res.status(200).json({ message: users, status: 200 });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+};
+
 export default {
   createUser,
   login,
+  getUsers,
 };
