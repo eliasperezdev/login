2fe4e0c7714f1d936948ef5794cf4adc01e16783
elias <perexlito@gmail.com>
2022-10-31 16:39:35 -0300

    move to auth

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,12 +1,9 @@
 import express from 'express';
-import controller from '../controllers/userController';
-import validateToken from '../middlewares/auth';
-import verifyAdmin from '../middlewares/verifyAdmin';
+import controller from '../controllers/authController';
 
 const router : express.Router = express.Router();
 
 router.post('/register', controller.createUser);
 router.post('/login', controller.login);
-router.get('/', validateToken, verifyAdmin, controller.getUsers);
 
 export default router;
